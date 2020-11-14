671a1fa2e63e093e1b1e06cfc0cec45307944662
Liapede <syuupon-1204@ezweb.ne.jp>
2020-11-15 00:51:48 +0900

    itemsページを作成

---
+++ lib/screen/items_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ItemsPage(),
+    ),
+  );
+}
+
+class ItemsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('二次関数'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              ButtonTheme(
+                minWidth: 600.0,
+                height: 50.0,
+                child: FlatButton(
+                  child: const Text('公式集'),
+                  shape: const UnderlineInputBorder(),
+                  color: Colors.white,
+                  textColor: Colors.blue,
+                  onPressed: () {},
+                ),
+              ),
+              ButtonTheme(
+                minWidth: 600.0,
+                height: 50.0,
+                child: FlatButton(
+                  child: const Text('平方完成'),
+                  shape: const UnderlineInputBorder(),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+              ),
+              ButtonTheme(
+                minWidth: 600.0,
+                height: 50.0,
+                child: FlatButton(
+                  child: const Text('最大最小'),
+                  shape: const UnderlineInputBorder(),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
