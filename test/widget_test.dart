bb3f286da64ced65509ca5166cd01306db2329e8
git_tutorial <m18007mm@gmail.com>
2020-11-14 17:20:31 +0900

    question delete

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:benesse_project_demo/screen/main_screen.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Quizzler());
+    await tester.pumpWidget(math_app());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
