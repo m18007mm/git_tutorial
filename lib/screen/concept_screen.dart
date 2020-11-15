39dc9e8cc7d0195877aaa1de3df6220756cb5895
Liapede <syuupon-1204@ezweb.ne.jp>
2020-11-15 11:05:19 +0900

    items-conceptの遷移

--- lib/screen/concept_screen.dart
+++ lib/screen/concept_screen.dart
@@ -1,1 +1,23 @@
 // TODO: とにかく実装
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Concept_screen(),
+    ),
+  );
+}
+
+class Concept_screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('解の公式'),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
--- lib/screen/items_screen.dart
+++ lib/screen/items_screen.dart
@@ -1,21 +1,22 @@
+import 'package:benesse_project_demo/screen/concept_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: ItemsPage(),
+      home: Items_screen(),
     ),
   );
 }
 
 // TODO: UIを整える
-class ItemsPage extends StatelessWidget {
+class Items_screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
+      //backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        //backgroundColor: Colors.blue,
         title: Text('二次関数'),
       ),
       body: SafeArea(
@@ -26,12 +27,11 @@ class ItemsPage extends StatelessWidget {
                 minWidth: 600.0,
                 height: 50.0,
                 child: FlatButton(
-                  child: const Text('公式集'),
-                  shape: const UnderlineInputBorder(),
-                  color: Colors.white,
-                  textColor: Colors.blue,
-                  onPressed: () {},
-                ),
+                    child: const Text('公式集'),
+                    shape: const UnderlineInputBorder(),
+                    color: Colors.white,
+                    textColor: Colors.blue,
+                    onPressed: () {}),
               ),
               ButtonTheme(
                 minWidth: 600.0,
@@ -40,7 +40,16 @@ class ItemsPage extends StatelessWidget {
                   child: const Text('解の公式'),
                   shape: const UnderlineInputBorder(),
                   color: Colors.white,
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return Concept_screen();
+                        },
+                      ),
+                    );
+                  },
                 ),
               ),
               ButtonTheme(
